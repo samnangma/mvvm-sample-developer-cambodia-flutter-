5b28825721a763dee4b609926ab21ee8aeb1bbc4
Samnang MA <masamnang6@gmail.com>
2023-11-13 22:46:02 +0700

    fix splash_screen

--- lib/views/splash_screen/splash_screen.dart
+++ lib/views/splash_screen/splash_screen.dart
@@ -1,48 +1,67 @@
-import 'dart:async';
-import 'package:developer_cambodia/views/login/login_screen.dart';
 import 'package:developer_cambodia/views/splash_screen/onboardingpage/onboarding_page.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-
+import 'package:developer_cambodia/views/home/home.dart';
+import 'package:developer_cambodia/views/login/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
-
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2), // Animation duration
+    );
 
-    Timer(const Duration(seconds: 3),
-            ()=> Navigator.push(
-            context, MaterialPageRoute(builder: (context) => OnboardingScreen())));
+    _controller.forward().then((value) {
+      // Navigate to the main screen after the animation
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) =>
+              OnboardingScreen(), // Replace 'HomeScreen()' with your main screen
+        ),
+      );
+    });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body:Stack(
-        alignment: Alignment.center,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/logos/splash.png')
-              )
+      body: Center(
+        child: FadeTransition(
+          opacity: _controller,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    width: 300,
+                    height: 300,
+                    child: Image.asset(
+                        'assets/logos/logo.png')), // This should display the image
+                SizedBox(height: 20),
+                Text(
+                  'Developers Cambodia',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ],
             ),
           ),
-          Positioned(
-              bottom: 50 ,
-              child: CupertinoActivityIndicator(color: Colors.white,))
-        ],
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
